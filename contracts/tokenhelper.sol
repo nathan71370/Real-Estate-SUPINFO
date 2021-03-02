e8f47214b5ee80eae27882c2f3cb9408fe8656aa
Nathan Mercier <nathanmercier@orange.fr>
2021-03-02 20:04:50 +0100

    bug fixes

--- contracts/tokenhelper.sol
+++ contracts/tokenhelper.sol
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >=0.7.0;
+pragma abicoder v2;
 
 import "./therealestate.sol";
 
 contract TokenHelper is TheRealEstate {
     
     function withdraw() external isOwner() {
-        address payable _owner = getOwner();
-        _owner.transfer(address(this).balance);
+        address payable owner = getOwner();
+        owner.transfer(address(this).balance);
     }
     
     function changeName(uint _tokenId, string calldata _newName) external onlyOwnerOf(_tokenId) {
@@ -34,6 +35,15 @@ contract TokenHelper is TheRealEstate {
         commissionPercent = _newCommision;
     }
     
+    function editToken(uint _tokenId, string calldata _newName, string calldata _newAdress, string calldata _description, uint16 _price, string calldata _newImageLink) external onlyOwnerOf(_tokenId) {
+        tokens[_tokenId].name = _newName;
+        tokens[_tokenId].adress = _newAdress;
+        tokens[_tokenId].price = _price;
+        tokens[_tokenId].description = _description;
+        tokens[_tokenId].imageLink = _newImageLink;
+        
+    }
+    
     function getTokensByOwner(address _owner) external view returns(uint[] memory) {
         uint[] memory result = new uint[](ownerTokenCount[_owner]);
         uint counter = 0;
@@ -58,4 +68,28 @@ contract TokenHelper is TheRealEstate {
         return result;
     }
     
+    function getAllTokensOnSale() external view returns(uint[] memory) {
+        return tokensOnSale;
+    }
+    
+    
+    function getName(uint _tokenId) external view returns(string memory){
+        return tokens[_tokenId].name;
+    }
+    
+    function getAdress(uint _tokenId) external view returns(string memory){
+        return tokens[_tokenId].adress;
+    }
+    
+    function getDescription(uint _tokenId) external view returns(string memory){
+        return tokens[_tokenId].description;
+    }
+    
+    function getPrice(uint _tokenId) external view returns(uint){
+        return tokens[_tokenId].price;
+    }
+    
+    function getImageLink(uint _tokenId) external view returns(string memory){
+        return tokens[_tokenId].imageLink;
+    }
 }
